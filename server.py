4dfa710ad87531158b7ef6b3cd28f879163ef475
Binh Giang <ndbghdvn@gmail.com>
2023-07-21 15:55:15 +0700

    testing bug

--- server.py
+++ server.py
@@ -1182,3 +1182,4 @@ if __name__ == "__main__":
                 create_interface()
     except KeyboardInterrupt as e:
         logger.error('\n'.join(traceback.format_exception(e)))
+        print('\n'.join(traceback.format_exception(e)))
